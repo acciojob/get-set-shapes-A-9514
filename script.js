e261d4211e53e471c2cd42b849d104879b903f45
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-17 14:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-//complete this code
-class Rectangle {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Square extends Animal {}
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
